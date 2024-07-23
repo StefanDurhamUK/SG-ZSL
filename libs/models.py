1443fad1d62c0003153f194bd026a235445d167b
StefanDurhamUK <fan.wan.uk@gmail.com>
2024-07-23 21:45:29 +0100

    Update models.py

--- libs/models.py
+++ libs/models.py
@@ -5,31 +5,9 @@ from args import Args
 
 args = Args().parse()
 
-"""
-# 2048-->1024-->512-->40/50
-class Teacher(nn.Module):
-    def __init__(self, input_dim, hidden_dim1, hidden_dim2, output_dim, dropout=0.5):
-        super(Teacher, self).__init__()
-        self.bn1 = nn.BatchNorm1d(hidden_dim1)
-        self.bn2 = nn.BatchNorm1d(hidden_dim2)
-        self.net = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim1),
-            self.bn1,
-            nn.LeakyReLU(),
-            nn.Dropout(p=dropout),
-            nn.Linear(hidden_dim1, hidden_dim2),
-            self.bn2,
-            nn.LeakyReLU(),
-            nn.Dropout(p=dropout),
-            nn.Linear(hidden_dim2, output_dim),
-        )
-
-    def forward(self, x):
-        output = self.net(x)
-        return output
-"""
 
 
+# Teacher Network
 class Teacher(nn.Module):
     def __init__(self, input_dim, hidden_dim1, hidden_dim2, output_dim, dropout=0.5):
         super(Teacher, self).__init__()
@@ -40,18 +18,6 @@ class Teacher(nn.Module):
         self.bn1 = nn.BatchNorm1d(hidden_dim1)
         self.bn2 = nn.BatchNorm1d(hidden_dim2)
         self.dt = nn.Dropout(p=dropout)
-        # self.hidden2 = hidden_dim2
-        # self.hidden = self.init_hidden()
-
-    """ 
-    def obtain_center(self, x):
-        inter_output = self.fc2(self.dt(self.lRelu(self.bn1(self.fc1(x)))))  # (batch,512)
-        return inter_output
-    """
-    """
-    def init_hidden(self):
-        return (torch.zeros(args.batchTS, self.hidden2))
-    """
 
     def forward(self, x):
         # inter_output = self.init_hidden()
@@ -64,21 +30,11 @@ class Teacher(nn.Module):
         data = self.lRelu(data)
         data = self.dt(data)
         output = self.fc3(data)
-        #inter_output = self.fc2(self.dt(self.lRelu(self.bn1(self.fc1(x)))))  # (batch,512)
-        #output = self.fc3(self.dt(self.lRelu(self.bn2(inter_output))))  # (batch, 50/40)
         return output
 
 
-# center loss method 1
-# 1.Train teacher: hidden_layer2(batch_size,512)-->center loss + ctn_loss
-# 2.Get Teacher's hidden_layer2(batch_size, 512)-->get mean 512 according to each cls
-# 3.Train generator-->2048-->teacher--> crossEntropyloss + [each cls: (batch_size,512)-mean 512]
 
-# 1.训练完teacher，把数据按一个batch送进去，得到（16000，512）
-# 2.（16000，512）按类别计算中心点（按类别求个平均的512），并存取（50/40，512）
-# 3. 训练generator：768+nl--》1024--》2048--》teacher（取fc2的512（batch，512）
-# 遍历batch，按类别将每个512-第二部中的512）【20，512】--》mse（【512】-【512】）+【20】loss：item
-
+# Generator 
 # 768+nl -->1024--2048
 class Generator(nn.Module):
     def __init__(self, att_dim, noise_dim, hidden_dim, output_dim, dropout=0.2):
@@ -135,7 +91,7 @@ class Generator(nn.Module):
         output = self.net(output)
         return output
 
-
+# Student Network
 class Student(nn.Module):
     def __init__(self, input_dim, hidden_dim1, hidden_dim2, output_dim, dropout=0.2):
         super(Student, self).__init__()
@@ -162,7 +118,7 @@ class Student(nn.Module):
         output = self.net(x)
         return output
 
-
+# Z Net(for zsl test)
 class Z_net(nn.Module):
     def __init__(self, input_dim, hidden_dim1, hidden_dim2, output_dim, dropout=0.5):
         super(Z_net, self).__init__()
